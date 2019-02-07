06c8038610298783ff84f500dc4b61a63921a6ba
Leigh Johnston <leigh@i42.co.uk>
2019-02-07 21:08:25 +0000

    Initial commit.

---
+++ include/neos/neos.hpp
@@ -0,0 +1,26 @@
+/*
+  neos.hpp
+
+  Copyright (c) 2019 Leigh Johnston.  All Rights Reserved.
+
+  This program is free software: you can redistribute it and / or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <neolib/neolib.hpp>
+#include <neolib/version.hpp>
+
+namespace neos
+{
+	const neolib::version NEOS_VERSION = { 1u, 0u, 0u, 0u, "Jaffa Cake"};
+}
